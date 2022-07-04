c5fe7b33a1526a3351e03f679f29a1b2217720a9
SnowLynns <20019791@myrp.edu.sg>
2022-07-04 12:37:13 +0800

    added meta description

--- web/test.php
+++ web/test.php
@@ -7,6 +7,7 @@
 <title>Crys's Herokuphp (I do not know what i am doing.)</title>
 <!-- Does not have a <meta name="viewport"> tag with width or initial-scaleNo `<meta name="viewport">` tag found -->
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="please let me 100 everything">
 </head>
 <body>
   Hello World!
