0ec25c4b6306578d74d2a58d17312e8dda201454
SnowLynns <20019791@myrp.edu.sg>
2022-07-04 12:14:53 +0800

    edited test.php

--- test.php
+++ test.php
@@ -1,5 +1,12 @@
-<html>
+<!-- fix: Page lacks the HTML doctype -->
+<!DOCTYPE html>
+<!-- fix : <html> element does not have a [lang] attribute -->
+<html lang="en">
 <head>
+    <!-- fix: Document doesn't have a <title> element -->
+<title>Crys's Herokuphp (I do not know what i am doing.)</title>
+<!-- Does not have a <meta name="viewport"> tag with width or initial-scaleNo `<meta name="viewport">` tag found -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
   Hello World!
